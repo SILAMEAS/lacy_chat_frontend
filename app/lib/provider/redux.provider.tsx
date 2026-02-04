589be2c9038a76f5d055bb7be2d88d67f747dcd7
Sila MEAS <sila.meas@allweb.com.kh>
2026-02-04 11:33:44 +0700

    fix-deploy-vecerl

--- app/lib/provider/redux.provider.tsx
+++ app/lib/provider/redux.provider.tsx
@@ -1,15 +1,13 @@
-// redux.provider.tsx
-import  {type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "~/redux/store";
 
 interface ReduxProviderProps {
   children: ReactNode;
 }
 
-const ReduxProvider: React.FC<ReduxProviderProps> = ({ children }) => {
-  // Only wrap with PersistGate if persistor exists (browser)
+const ReduxProvider = ({ children }: ReduxProviderProps) => {
   if (persistor) {
     return (
       <Provider store={store}>
@@ -20,7 +18,6 @@ const ReduxProvider: React.FC<ReduxProviderProps> = ({ children }) => {
     );
   }
 
-  // Server-side or SSR fallback (no PersistGate)
   return <Provider store={store}>{children}</Provider>;
 };
 
